8f8dcbeb0b3ca3dbfd7d0a899ad20ba1e3acc8dd
zhouxin901008 <zhouxin@lsh123.com>
2017-08-11 17:50:15 +0800

    update

--- base/db.py
+++ base/db.py
@@ -38,56 +38,6 @@ class db:
             mysqldb.rollback()
         mysqldb.close()
 
-    def  getPaymentConnection(self):
-        cf = ConfigParser.ConfigParser()
-        cf.read("/Users/zhouxin/PycharmProjects/testinterface/com/java/conf/db_config.ini")
-        HOST = cf.get("dbconf_payment", "db_host")
-        PORT = cf.get("dbconf_payment", "db_port")
-        USER = cf.get("dbconf_payment", "db_user")
-        PW = cf.get("dbconf_payment","db_password")
-        DB = cf.get("dbconf_payment", "db_name")
-        dbConnect = MySQLdb.Connect(host=HOST, port=int(PORT), user=USER, passwd=PW, db=DB,charset="utf8")
-        return dbConnect
-
-    def paymentQuery(self,sql):
-        mysql = db()
-        mysqldb = mysql.getPaymentConnection()
-        cursor = mysqldb.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-        try:
-            cursor.execute(sql)
-            s = cursor.fetchall()
-            #print s
-        except:
-            print "Error: unable to fecth data"
-        mysqldb.close()
-        return s
-
-    def  getTmsConnection(self):
-        cf = ConfigParser.ConfigParser()
-        cf.read("/Users/zhouxin/PycharmProjects/testinterface/com/java/conf/db_config.ini")
-        HOST = cf.get("dbconf_lsh_tms", "db_host")
-        PORT = cf.get("dbconf_lsh_tms", "db_port")
-        USER = cf.get("dbconf_lsh_tms", "db_user")
-        #PW = cf.get("dbconf_lsh_tms","db_password")
-        DB = cf.get("dbconf_lsh_tms", "db_name")
-        dbConnect = MySQLdb.Connect(host=HOST, port=int(PORT), user=USER, db=DB,charset="utf8")
-        return dbConnect
-
-    def tmsQuery(self,sql):
-        mysql = db()
-        mysqldb = mysql.getTmsConnection()
-        cursor = mysqldb.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-        try:
-            cursor.execute(sql)
-            s = cursor.fetchall()
-            #print s
-        except:
-            print "Error: unable to fecth data"
-        mysqldb.close()
-        return s
-
-
-
 
 #a = db()
 #a.query("select * from user_info limit 1")
